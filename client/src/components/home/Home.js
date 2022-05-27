e14cfa657a5eca32966d084cf7668b3e9389178d
willywin99 <willywinsianturi@gmail.com>
2022-05-27 22:50:41 +0700

    Add a create room form

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../UserContext';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const {user, setUser} = useContext(UserContext);
+  const setAsJohn = () => {
+    const john = {
+      name: 'John',
+      email: 'john@email.com',
+      password: '123',
+      id: '123',
+    }
+    setUser(john);
+  };
+  const setAsTom = () => {
+    const tom = {
+      name: 'Tom',
+      email: 'tom@email.com',
+      password: '456',
+      id: '456',
+    }
+    setUser(tom);
+  }
+
   return (
     <div>
-      <h1>Home</h1>
+      <div className="row">
+        <div className="col s12 m6">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Welcome {user? user.name: ''}</span>
+              <form>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input id="room" type="text" className="validate" placeholder='Enter a room name'/>
+                    <label htmlFor="room">Room</label>
+                  </div>
+                </div>
+                <button className='btn'>Create Room</button>
+              </form>
+            </div>
+            <div class="card-action">
+              <a href="#" onClick={setAsJohn}>set as John</a>
+              <a href="#" onClick={setAsTom}>set as Tom</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Link to={'/chat'}>
+        <button>go to chat</button>
+      </Link>
     </div>
   )
 }
