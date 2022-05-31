47bf6fd7c24ead58d3d4582ad52d379ea0bfdbc5
willywin99 <willywinsianturi@gmail.com>
2022-05-31 19:42:27 +0700

    Prepare for the server side

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
+const app = require('express')();
+const http = require('http').createServer(app);
 
+const socketio = require('socket.io');
+const io = socketio(http);
+
+const PORT = process.env.PORT || 5000
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
