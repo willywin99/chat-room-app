5f524f9c03c2b82b09deea3280d0a9d4e07fcb7f
willywin99 <willywinsianturi@gmail.com>
2022-06-03 02:04:50 +0700

    Receive the room name at the server side

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const io = socketio(http);
 const PORT = process.env.PORT || 5000
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(socket.id);
+  socket.on('create-room', name=> {
+    console.log('Then room name received is ', name);
+  });
 });
 
 http.listen(PORT, () => {
