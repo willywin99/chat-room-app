e7f8565ae6d7db5f7b4fac6cdeae8855ce7287bd
willywin99 <willywinsianturi@gmail.com>
2022-06-10 05:53:23 +0700

    Add send message function

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../UserContext';
 import { Link, useParams } from 'react-router-dom';
 
@@ -11,18 +11,34 @@ const Chat = () => {
   const {user, setUser} = useContext(UserContext);
   let {room_id, room_name} = useParams();
 
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     socket = io(ENDPOINT);
     socket.emit('join', {name: user.name, room_id, user_id: user.id})
   }, [])
 
+  const sendMessage = event => {
+    event.preventDefault();
+    if (message) {
+      console.log(message);
+      socket.emit('sendMessage', message, room_id, () => setMessage(''));
+    }
+  }
+
   return (
     <div>
       <div>{room_id} {room_name}</div>
       <h1>Chat {JSON.stringify(user)} </h1>
-      <Link to={'/'}>
-        <button>go to home</button>
-      </Link>
+      <form action="" onSubmit={sendMessage}>
+        <input 
+          type="text"
+          value={message}
+          onChange={event=>setMessage(event.target.value)}
+          onKeyPress={event=>event.key === 'Enter' ? sendMessage(event) : null}
+        />
+        <button>Send Message</button>
+      </form>
     </div>
   )
 }
