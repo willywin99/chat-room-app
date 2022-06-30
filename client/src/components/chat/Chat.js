88260a83704670595b00cbbc2d0452be260d7a28
willywin99 <willywinsianturi@gmail.com>
2022-06-30 08:30:03 +0700

    Create message component

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,55 +1,48 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../UserContext';
 import { Link, useParams } from 'react-router-dom';
-
 import io from 'socket.io-client';
+import Messages from './messages/Messages';
 let socket;
-
 const Chat = () => {
-  const ENDPOINT = 'localhost:5000';
+    const ENDPT = 'localhost:5000';
 
-  const {user, setUser} = useContext(UserContext);
-  let {room_id, room_name} = useParams();
-
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-
-  useEffect(() => {
-    socket = io(ENDPOINT);
-    socket.emit('join', {name: user.name, room_id, user_id: user.id})
-  }, [])
-
-  useEffect(() => {
-    socket.on('message', message => {
-      setMessages([...messages, message])
-    })
-  }, [messages])
-  
-
-  const sendMessage = event => {
-    event.preventDefault();
-    if (message) {
-      console.log(message);
-      socket.emit('sendMessage', message, room_id, () => setMessage(''));
+    const { user, setUser } = useContext(UserContext);
+    let { room_id, room_name } = useParams();
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        socket = io(ENDPT);
+        socket.emit('join', { name: user.name, room_id, user_id: user.id })
+    }, [])
+    useEffect(() => {
+        socket.on('message', message => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+    const sendMessage = event => {
+        event.preventDefault();
+        if (message) {
+            console.log(message)
+            socket.emit('sendMessage', message, room_id, () => setMessage(''))
+        }
     }
-  }
-
-  return (
-    <div>
-      <div>{room_id} {room_name}</div>
-      <h1>Chat {JSON.stringify(user)} </h1>
-      <pre>{JSON.stringify(messages, null, '\t')}</pre>
-      <form action="" onSubmit={sendMessage}>
-        <input 
-          type="text"
-          value={message}
-          onChange={event=>setMessage(event.target.value)}
-          onKeyPress={event=>event.key === 'Enter' ? sendMessage(event) : null}
-        />
-        <button>Send Message</button>
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <div>{room_id} {room_name}</div>
+            <h1>Chat {JSON.stringify(user)}</h1>
+            <pre>{JSON.stringify(messages, null, '\t')}</pre>
+            {/* <Messages messages={messages} user_id={user.id} /> */}
+            <form action="" onSubmit={sendMessage}>
+                <input type="text"
+                    value={message}
+                    onChange={event => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+                <button>Send Message</button>
+            </form>
+        </div>
+    )
 }
 
 export default Chat
--- server/index.js
+++ server/index.js
@@ -1,65 +1,62 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const mongoose = require('mongoose');
-
-const socketio = require('socket.io');
+const socketio = require('socket.io')
 const io = socketio(http);
-
 const mongoDB = "mongodb+srv://ckmobile:ckmobile123@cluster0.fykanxv.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('connected'))
-  .catch(err => console.log(err));
-
-const {addUser, getUser, removeUser} = require('./helper');
-
-const PORT = process.env.PORT || 5000
-
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('connected')).catch(err => console.log(err))
+const { addUser, getUser, removeUser } = require('./helper');
+const Message = require('./models/Message');
+const PORT = process.env.PORT || 5000;
 const Room = require('./models/Room');
 
 io.on('connection', (socket) => {
-  console.log(socket.id);
-  Room.find().then(result => {
-    console.log('output-rooms', result)
-    socket.emit('output-rooms', result)
-  })
-  socket.on('create-room', name=> {
-    // console.log('Then room name received is ', name);
-    const room = new Room({name});
-    room.save().then(result => {
-      io.emit('room-created', result)
-    });
-  });
-  socket.on('join', ({name, room_id, user_id}) => {
-    const {error, user} = addUser({
-      socket_id: socket.id,
-      name,
-      room_id,
-      user_id
+    console.log(socket.id);
+    Room.find().then(result => {
+        socket.emit('output-rooms', result)
     })
-    socket.join(room_id);
-    if (error) {
-      console.log('join error', error);
-    } else {
-      console.log('join user', user);
-    }
-  })
-  socket.on('sendMessage', (message, room_id, callback) => {
-    const user = getUser(socket.id);
-    const msgToStore = {
-      name: user.name,
-      user_id: user.user_id,
-      room_id,
-      text: message
-    }
-    console.log('message', msgToStore);
-    io.to(room_id).emit('message', msgToStore);
-    callback();
-  })
-  socket.on('disconnect', () => {
-    const user = removeUser(socket.id);
-  })
+    socket.on('create-room', name => {
+        // console.log('Then room name received is ', name)
+        const room = new Room({ name });
+        room.save().then(result => {
+            io.emit('room-created', result)
+        })
+    })
+    socket.on('join', ({ name, room_id, user_id }) => {
+        const { error, user } = addUser({
+            socket_id: socket.id,
+            name,
+            room_id,
+            user_id
+        })
+        socket.join(room_id);
+        if (error) {
+            console.log('join error', error)
+        } else {
+            console.log('join user', user)
+        }
+    })
+    socket.on('sendMessage', (message, room_id, callback) => {
+        const user = getUser(socket.id);
+        const msgToStore = {
+            name: user.name,
+            user_id: user.user_id,
+            room_id,
+            text: message
+        }
+        console.log('message', msgToStore)
+        const msg = new Message(msgToStore);
+        msg.save().then(result => {
+            io.to(room_id).emit('message', result);
+            callback()
+        })
+
+    })
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+    })
 });
 
 http.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
+    console.log(`listening on port ${PORT}`);
 });
