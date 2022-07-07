10274e392b25ccfc300abcc7c29a143a8c5d043f
willywin99 <willywinsianturi@gmail.com>
2022-07-07 21:17:03 +0700

    Get the values from the error object

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,4 +1,16 @@
 const User = require('../models/User');
+const alertError = (err) => {
+  let errors = {name:'', email:'', password:''};
+  // console.log(`error message: ${err.message}`);
+  // console.log(`error code: ${err.code}`);
+  // console.log(err);
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({properties}) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+}
 
 module.exports.signup = async (req, res) => {
   console.log('req.body', req.body);
@@ -7,9 +19,8 @@ module.exports.signup = async (req, res) => {
     const user = await User.create({name, email, password});
     res.status(201).json({user});
   } catch (error) {
-    console.log(`error message: ${error.message}`);
-    console.log(`error code: ${error.code}`);
-    // res.status(400).send('Fail to create user');
+    let errors = alertError(error);
+    res.status(400).json({errors});
   }
 }
 
