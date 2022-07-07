4a60ed0ee5e165d7ea08a4b3abb5e46846e522d9
willywin99 <willywinsianturi@gmail.com>
2022-07-07 14:42:02 +0700

    Add routes and controllers to server

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+const router = Router();
+const authController = require('../controllers/authControllers');
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+
+module.exports = router;
