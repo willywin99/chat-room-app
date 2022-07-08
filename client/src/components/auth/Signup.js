77c8ab37cfc7feee6890f370e9481e5b91abaf8c
willywin99 <willywinsianturi@gmail.com>
2022-07-08 07:43:41 +0700

    Add the function and error message to signup page

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log(name, email, password);
+  }
   return (
     <div className="row">
-      <form className="col s12">
+      <form className="col s12" onSubmit={submitHandler}>
         <div className="row">
           <div className="input-field col s12">
-            <input id="name" type="text" className="validate" />
+            <input 
+              id="name" 
+              type="text" 
+              className="validate"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <div className="name error red-text">Name is required</div>
             <label htmlFor="name">Name</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <input id="email" type="email" className="validate" />
+            <input 
+              id="email" 
+              type="email" 
+              className="validate"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <div className="email error red-text">Email is required</div>
             <label htmlFor="email">Email</label>
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <input id="password" type="password" className="validate" />
+            <input 
+              id="password" 
+              type="password" 
+              className="validate"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <div className="password error red-text">Password is required</div>
             <label htmlFor="password">Password</label>
           </div>
         </div>
