25a5b3958644b701dc8bd46b76f0a8cb0b5481c7
willywin99 <willywinsianturi@gmail.com>
2022-07-08 07:16:25 +0700

    Create route to Login and Signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React, {useState} from 'react';
 import {UserContext} from './UserContext';
+
 import Chat from './components/chat/Chat';
 import Home from './components/home/Home';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -15,7 +18,9 @@ function App() {
           <Navbar/>
           <Routes>
             <Route exact path='/' element={<Home/>} />
-            <Route exact path='/chat/:room_id/:room_name' element={<Chat/>} />
+            <Route path='/chat/:room_id/:room_name' element={<Chat/>} />
+            <Route path='/signup' element={<Signup/>} />
+            <Route path='/login' element={<Login/>} />
           </Routes>
         </UserContext.Provider>
       </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,15 +9,15 @@ const Navbar = () => {
           <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#">Login</a></li>
-            <li><a href="#">Signup</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/signup">Signup</a></li>
             <li><a href="#">Logout</a></li>
           </ul>
         </div>
       </nav>
       <ul className="sidenav" id="mobile-demo">
-        <li><a href="sass.html">Login</a></li>
-        <li><a href="badges.html">Signup</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/signup">Signup</a></li>
         <li><a href="collapsible.html">Logout</a></li>
       </ul>
     </>
