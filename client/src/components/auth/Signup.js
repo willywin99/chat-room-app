1c9139adb85e7c1f8a2551a5bde89e6c44bc9564
willywin99 <willywinsianturi@gmail.com>
2022-07-08 07:29:29 +0700

    Detail the signup page

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="row">
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="name" type="text" className="validate" />
+            <label htmlFor="name">Name</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="email" type="email" className="validate" />
+            <label htmlFor="email">Email</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="password" type="password" className="validate" />
+            <label htmlFor="password">Password</label>
+          </div>
+        </div>
+        <button className="btn">Signup</button>
+      </form>
     </div>
+
   )
 }
 
