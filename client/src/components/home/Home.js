01f36cc7888d53f5092c7c01dccb1913fff4eb3a
willywin99 <willywinsianturi@gmail.com>
2022-07-12 13:23:00 +0700

    Finally remove unnecessary functions

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -40,24 +40,6 @@ const Home = () => {
 
     }
 
-    const setAsJohn = () => {
-        const john = {
-            name: 'John',
-            email: 'john@email.com',
-            password: '123',
-            id: '123'
-        }
-        setUser(john);
-    }
-    const setAsTom = () => {
-        const tom = {
-            name: 'Tom',
-            email: 'tom@email.com',
-            password: '456',
-            id: '456'
-        }
-        setUser(tom);
-    }
     if (!user) {
         return <Navigate to='/login' />
     }
@@ -84,8 +66,7 @@ const Home = () => {
                             </form>
                         </div>
                         <div className="card-action">
-                            <a href="#" onClick={setAsJohn}>set as John</a>
-                            <a href="#" onClick={setAsTom}>set as Tom</a>
+                            
                         </div>
                     </div>
                 </div>
